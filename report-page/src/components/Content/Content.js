a86aef5376e3cfc5b36a3c15cea22e6a25f995fd
Sergio F. Rodriguez <serrod@gmail.com>
2020-02-07 16:20:32 +0100

    working on styles and layout - issues with ms-Grid

--- report-page/src/components/Content/Content.js
+++ report-page/src/components/Content/Content.js
@@ -4,16 +4,12 @@ import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar'
 import { Selection, SelectionMode } from 'office-ui-fabric-react/lib/utilities/selection'
 import './Content.css'
 import { Depths } from '@uifabric/fluent-theme/lib/fluent/FluentDepths';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
-import { Grid } from 'office-ui-fabric-react/lib/Grid';
-import { DatePicker } from 'office-ui-fabric-react/lib/DatePicker';
-
 
 let  breadcrumbs = [
-  {key:1, text: 'Home>', onClick: console.log('here')},
-  {key:2, text: 'Reports>', onClick: console.log('here')},
-  {key:3, text: 'J3>', onClick: console.log('here')},
-  {key:4, text: 'J33>', onClick: console.log('here')},
+  {key:1, text: 'Home', onClick: console.log('here')},
+  {key:2, text: 'Reports', onClick: console.log('here')},
+  {key:3, text: 'J3', onClick: console.log('here')},
+  {key:4, text: 'J33', onClick: console.log('here')},
   {key:5, text: 'CCIR', onClick: 5},
 ]
 
@@ -63,31 +59,25 @@ export default class Content extends Component {
     }
     
     Body = ({groups, expanded, collapsed}) => (
-      <div className="container" class="ms-Grid" dir="ltr">
-        <Breadcrumb className="breadcrumbs" 
-          items={breadcrumbs} 
-          maxDisplayedItems={5}
-        />
-        <div style={{ boxShadow: Depths.depth8 }}>
-          <CommandBar isSearchBoxVisible={true} 
-            items = {breadcrumbs} 
-            overflowItems={breadcrumbs} 
-            overflowButtonProps={this.createListItems} 
-            farItems={createListItems} 
-            ariaLabel="Use left and right arrow keys to navigate between commands"/>
-        </div>
-        <DefaultButton
-          text='See Button'
-          primary={ true }
-          href='#/components/button'
-        />
-        <div class="ms-Grid" dir="ltr">
-          <div class="ms-Grid-row" style={{Grid}}>
-            <div class="ms-Grid-col ms-sm6 ms-md4 ms-lg2"> <DatePicker /></div>
-            <div class="ms-Grid-col ms-sm6 ms-md8 ms-lg10">B</div>
+
+      <div class="ms-Fabric" dir="ltr">  
+      <div class="ms-Grid">
+        <div class="ms-Grid-row">
+          <Breadcrumb className="breadcrumbs" items={breadcrumbs} maxDisplayedItems={5} />
+          <div style={{ boxShadow: Depths.depth8 }}>
+            <CommandBar isSearchBoxVisible={true} 
+              items = {breadcrumbs} 
+              overflowItems={breadcrumbs} 
+              overflowButtonProps={this.createListItems} 
+              farItems={createListItems} 
+              ariaLabel="Use left and right arrow keys to navigate between commands"/>
           </div>
+          <div class="ms-Grid-col"><div style={{ boxShadow: Depths.depth16 }}> WIDGET ONE </div></div>
+          <div class="ms-Grid-col"><div style={{ boxShadow: Depths.depth16 }}> WIDGET ONE </div></div>
         </div>
-      </div>  
+        </div>
+    </div>
+    
     )
 
     render() {
